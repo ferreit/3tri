8553a6b958b7c320d025a34151ab216aaa82f549
ferreit <105502286+ferreit@users.noreply.github.com>
2023-11-22 08:13:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+    
+}
+const listadeTeclas = document.querySelectorAll(".tecla");
+
+
+for(let contador = 0; contador < listadeTeclas.length; contador++){
+    const tecla = listadeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = `#som_${efeito}`; 
+    tecla.onclick = function(){
+        tocaSom(idAudio)
+    }
+   tecla.onkeydown = function (evento) {
+        if (evento.code != 'Tab'){
+            tecla.classList.add('ativa');
+        }
+        
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+}
+
